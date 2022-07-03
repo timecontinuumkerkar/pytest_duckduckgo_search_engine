8ea8af7b2ea60a3a05fc5b24c3678108c456d22f
timecontinuumkerkar <timeconitnuumkerkar@gmail.com>
2022-07-04 01:09:29 +0530

    First commit for pytest

---
+++ tests/test_search.py
@@ -0,0 +1,22 @@
+"""
+Basic Test Example for Pytest + Selenium Webdriver
+"""
+
+
+def test_basic_duckduckgo_search():
+    # Given That results are displayed for "panda"
+    # TODO
+
+    # When user searches for "panda"
+    # TODO
+
+    # Then the result title contains "panda"
+    # TODO
+
+    # AND the search query contains "panda"
+    # TODO
+
+    # AND the search result contains "panda"
+    # TODO
+
+    raise Exception("Incomplete Test")
